a82a0f5bf767af5349d55925c62fe4fbc06f9a87
Raj <rajvermasja18@gmail.com>
2023-07-23 07:37:03 +0530

    2nd question of string

---
+++ String encoding.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h> 
+string encode(string &message)
+{
+    //   Write your code here.
+    string ans;
+    int count =1;
+
+    char s=message[0];
+    for(int i=1;i<=message.length();i++){
+        if(message[i]==s){
+            count++;
+        }else{
+            ans+=message[i-1];
+            ans+=to_string(count);
+            count =1;
+            s=message[i];
+        }
+    }
+
+    return ans;
+}
