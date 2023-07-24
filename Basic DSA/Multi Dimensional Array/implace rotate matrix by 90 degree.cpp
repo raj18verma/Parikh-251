2045b700dad1443da92f43ad191e2e144fdfd535
Raj <rajvermasja18@gmail.com>
2023-07-24 10:58:00 +0530

    Day 3 question 3

---
+++ Basic DSA/Multi Dimensional Array/implace rotate matrix by 90 degree.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h> 
+void inplaceRotate(vector<vector<int>> &inputArray)
+{
+    // Write your code here.
+    int n=inputArray.size();
+    for (int i=0; i<n/2; i++){
+        for(int j=i; j<n-i-1; j++){
+            //store the first element
+            int temp=inputArray[i][j];
+
+            inputArray[i][j]=inputArray[j][n-1-i];
+
+            inputArray[j][n-1-i]=inputArray[n-1-i][n-1-j];
+
+            inputArray[n-1-i][n-1-j]=inputArray[n-1-j][i];
+
+            inputArray[n-1-j][i]=temp;
+
+        }
+    }
+}
