0ea6deecfb2ed2a638b85ae8645e8e4a8a501fbe
RAJ VERMA <104757394+raj18verma@users.noreply.github.com>
2023-07-28 11:17:17 +0530

    Update Non-decreasing array.cpp

--- Basic DSA/Arrays/Non-decreasing array.cpp
+++ Basic DSA/Arrays/Non-decreasing array.cpp
@@ -3,13 +3,18 @@ bool isPossible(int *arr, int n)
 {
     //  Write your code here
      int count = 0;             
-        for(int i = 1; i < n; i++){
-            if(arr[i] < arr[i-1]){
-                count++;
-                if(count > 1) return false;
-                if(i == 1 || arr[i-2] <= arr[i]) arr[i-1] = arr[i];    
-                else arr[i] = arr[i-1];
-            }
-        }
-        return true;
+     for(int i=0; i<n-1; i++){
+         if(arr[i]>arr[i+1]){
+             if(i>0){
+                 if(arr[i-1]<=arr[i+1])
+                 arr[i]=arr[i-1];
+                 else
+                 arr[i+1]=arr[i];
+             }
+             count++;
+         }
+     }
+     return count<=1; //(Return true only when count<=1 else return false)
 }
+
+
