6e195fc5faf8900b7102a449b4009ede9c26af60
RAJ VERMA <104757394+raj18verma@users.noreply.github.com>
2023-07-31 22:48:58 +0530

    Day 10 question 5 of basic algorithms

---
+++ Basic DSA/Basic Algortithms/Dutch National Flag Algorithm.cpp
@@ -0,0 +1,27 @@
+/* This algorithm contains 3 pointers i.e. low, mid, and high, and 3 main rules.  The rules are the following:
+
+arr[0….low-1] contains 0. [Extreme left part]
+arr[low….mid-1] contains 1.
+arr[high+1….n-1] contains 2. [Extreme right part], n = size of the array
+
+*/
+
+void sortArray(vector<int>& arr, int n) {
+
+    int low = 0, mid = 0, high = n - 1; // 3 pointers
+
+    while (mid <= high) {
+        if (arr[mid] == 0) {
+            swap(arr[low], arr[mid]);
+            low++;
+            mid++;
+        }
+        else if (arr[mid] == 1) {
+            mid++;
+        }
+        else {
+            swap(arr[mid], arr[high]);
+            high--;
+        }
+    }
+}
