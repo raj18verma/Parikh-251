baa1ab8dc208ef1ffac098786a141300a3b22c6d
RAJ VERMA <104757394+raj18verma@users.noreply.github.com>
2023-09-04 23:54:26 +0530

    Create Stack implementation using arrays.cpp

---
+++ Intermediate DSA/Stacks and Queues/Stack implementation using arrays.cpp
@@ -0,0 +1,47 @@
+// Question Link:- https://www.codingninjas.com/studio/problems/stack-implementation-using-array_3210209?utm_source=youtube&utm_medium=affiliate&utm_campaign=parikh_youtube
+
+// Stack class.
+class Stack {
+    
+public:
+    int *arr;
+    int top1;
+    int size;
+
+    Stack(int capacity) {
+        // Write your code here.
+        size=capacity;
+        top1=-1;
+        arr=new int[capacity];
+    }
+
+    void push(int num) {
+        // Write your code here.
+        if(!isFull())
+        arr[++top1]=num;
+    }
+
+    int pop() {
+        // Write your code here.
+        if(isEmpty()) return -1;
+        top1--;
+        return arr[top1+1];
+    }
+    
+    int top() {
+        // Write your code here.
+        if(isEmpty()) return -1;
+        return arr[top1];
+    }
+    
+    int isEmpty() {
+        // Write your code here.
+        return (top1==-1)? 1:0;
+    }
+    
+    int isFull() {
+        // Write your code here.
+        return (top1+1==size)? 1:0;
+    }
+    
+};
